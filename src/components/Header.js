6f7f42a9be5e4a945be3475faa176660bb02c600
Anupam Dubey <9430137649mk@gmail.com>
2022-02-15 23:10:09 +0530

    Updated dependencies

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,33 +3,33 @@ import BeenhereIcon from "@material-ui/icons/Beenhere";
 import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import { Button, MenuItem, Menu } from "@material-ui/core";
-import firebase from "firebase";
+// import firebase from "firebase";
 import styled from "styled-components";
 
 const Header = () => {
-	const [anchorEl, setAnchorEl] = useState(null);
+	// const [anchorEl, setAnchorEl] = useState(null);
 	const [auth, setAuth] = useState(false);
-	const open = Boolean(anchorEl);
+	// const open = Boolean(anchorEl);
 
-	const handleLogin = async () => {
-		// Connect to Google Authentication
-		const { provider } =  await import('./firebase.js')
-		firebase.auth().signInWithPopup(provider)
-			.then((response) => {
-				console.log(response)
-				setAuth(true);
-			})
-			.catch(err => alert(err))
-	}
+	// const handleLogin = async () => {
+	// 	// Connect to Google Authentication
+	// 	const { provider } =  await import('./firebase.js')
+	// 	firebase.auth().signInWithPopup(provider)
+	// 		.then((response) => {
+	// 			console.log(response)
+	// 			setAuth(true);
+	// 		})
+	// 		.catch(err => alert(err))
+	// }
 
-	const handleClose = () => {
-		setAnchorEl(null);
-		setAuth(false);
-	}
+	// const handleClose = () => {
+	// 	setAnchorEl(null);
+	// 	setAuth(false);
+	// }
 
-	const handleLogoutMenu = (event) => {
-		setAnchorEl(event.currentTarget);
-	}
+	// const handleLogoutMenu = (event) => {
+	// 	setAnchorEl(event.currentTarget);
+	// }
 
 	return (
 		<HeaderContainer>
@@ -43,20 +43,20 @@ const Header = () => {
 					aria-label="account of current user"
 					aria-controls="simple-menu"
 					aria-haspopup="true"
-					onClick={handleLogoutMenu}
+					// onClick={handleLogoutMenu}
 					style={{ height: '50px', minWidth: '55px' }}
 				>
 					<MenuIcon style={{ fill: "white", fontSize: '1.8rem', borderRadius: '7px' }} />
 				</Button> : <Button
 					aria-label="Login button"
 					aria-controls="login"
-					onClick={handleLogin}
+					// onClick={handleLogin}
 					style={{ height: '50px', minWidth: '55px' }}
 				>
 					<AccountCircle style={{ fill: "white", fontSize: '1.8rem', borderRadius: '7px' }} />
 				</Button>}
 
-				<Menu
+				{/* <Menu
 					id="logout-menu"
 					anchorEl={anchorEl}
 					keepMounted
@@ -64,7 +64,7 @@ const Header = () => {
 				>
 					<MenuItem onClick={handleClose}>Logout</MenuItem>
 					<MenuItem style={{ color: 'red'}}>Delete Account</MenuItem>
-				</Menu>
+				</Menu> */}
 			</NavMenu>
 		</HeaderContainer>
 	);
