99525d1519d8e85604c1516d794241a86eebdc0b
Anupam Dubey <9430137649mk@gmail.com>
2022-02-16 21:34:39 +0530

    Added new server url

--- src/constants/APIrequestUrl.js
+++ src/constants/APIrequestUrl.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://notex-backend.herokuapp.com/"
+export const BACKEND_URL = "https://notex-server.herokuapp.com/"
 // export const BACKEND_URL = "http://localhost:5000/"
